34948343c3d06021d4d6c7e7b84ed4a3f9634ff9
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-09-01 13:32:20 -0600

    updated project

--- controller.js
+++ controller.js
@@ -1,10 +1,32 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
-  $scope.getUsers = function() {
-    mainService.getUsers().then(function(dataFromService) {
-      $scope.users = dataFromService.data.data;
-    });
-  }
+  $scope.pageNum = 1;
+
+  // $scope.getUsers = function() {
+  //   mainService.getUsers($scope.pageNum).then(function(response) {
+  //     $scope.users = response.data.data;
+  //     $scope.total_pages = response.data.total_pages;
+  //   });
+  // };
+  // angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+    $scope.getUsers = function() {
+      mainService.getUsers().then(function(dataFromService) {
+        $scope.users = dataFromService;
+      });
+    };
 
   $scope.getUsers();
+
+  // $scope.previous = function() {
+  //   if($scope.pageNum > 1) {
+  //     $scope.pageNum--;
+  //     $scope.getUsers();
+  // }
+  // };
+  // $scope.next = function() {
+  //   if ($scope.total_pages > $scope.pageNum) {
+  //     $scope.pageNum++;
+  //     $scope.getUsers();
+  //   }
+  // };
 
 });
--- service.js
+++ service.js
@@ -1,10 +1,22 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) { // 1
+  this.getUsers = function(pageNum) {
+    var deferred = $q.defer(); // 2
+    $http({
+			method: 'GET',
+			url: 'http://reqres.in/api/users?page=' + pageNum
+		}).then(function(response) {              // 3
+      var parsedResponse = response.data.data;    // 4
+      deferred.resolve(parsedResponse);
+    });
+    return deferred.promise;
+  };
 
-  this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+
 
 });
+
+
+// 1. Add the ''$q' to the first line inside the function
+// 2. Add a deferred variable to handle it (line 3)
+// 3. Add a promise to the service, and remove the 'return' from line 4
+// 4. Use the $q to filter the data
